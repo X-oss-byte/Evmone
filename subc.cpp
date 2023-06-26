9c09d4a45997e40ab7ec934c2b152b30b98969ea
Paweł Bylica <pawel@ethereum.org>
2023-06-26 16:50:15 +0200

    subc_test reduce

--- subc.cpp
+++ subc.cpp
@@ -2,14 +2,14 @@
 
 using namespace intx;
 
-[[gnu::noinline]] static auto sub(const uint256& x, const uint256& y)
+[[gnu::noinline]] static auto sub(const uint128& x, const uint128& y)
 {
     return x - y;
 }
 
 int main()
 {
-    if (sub(0, 1) != ~uint256{})
+    if (sub(0, 1) != ~uint128{})
         return 1;
 
     return 0;
