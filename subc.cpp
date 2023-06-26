9e7698017236547f37735298d481ae402a338e28
Paweł Bylica <pawel@ethereum.org>
2023-06-26 17:10:08 +0200

    subc_test reduce

--- subc.cpp
+++ subc.cpp
@@ -1,39 +1,27 @@
 #include <intx/intx.hpp>
 
-using namespace intx;
+template <typename T>
+struct result_with_carry
+{
+    T value;
+    bool carry;
 
-inline constexpr result_with_carry<uint64_t> _subc(
+    /// Conversion to tuple of references, to allow usage with std::tie().
+    constexpr operator std::tuple<T&, bool&>() noexcept { return {value, carry}; }
+};
+
+inline result_with_carry<uint64_t> _subc(
     uint64_t x, uint64_t y, bool carry = false) noexcept
 {
-#if __has_builtin(__builtin_subcll)
-    if (!std::is_constant_evaluated())
-    {
-        unsigned long long carryout = 0;  // NOLINT(google-runtime-int)
-        const auto d = __builtin_subcll(x, y, carry, &carryout);
-        static_assert(sizeof(d) == sizeof(uint64_t));
-        return {d, static_cast<bool>(carryout)};
-    }
-#elif __has_builtin(__builtin_ia32_sbb_u64)
-    if (!std::is_constant_evaluated())
-    {
-        unsigned long long d = 0;  // NOLINT(google-runtime-int)
-        static_assert(sizeof(d) == sizeof(uint64_t));
-        const auto carryout = __builtin_ia32_sbb_u64(carry, x, y, &d);
-        return {d, static_cast<bool>(carryout)};
-    }
-#endif
-
-    const auto d = x - y;
-    const auto carry1 = x < y;
-    const auto e = d - carry;
-    const auto carry2 = d < uint64_t{carry};
-    return {e, carry1 || carry2};
+    unsigned long long carryout = 0;  // NOLINT(google-runtime-int)
+    const auto d = __builtin_subcll(x, y, carry, &carryout);
+    return {d, static_cast<bool>(carryout)};
 }
 
 
-[[gnu::noinline]] static auto neg(const uint192& y)
+[[gnu::noinline]] static auto neg(const intx::uint192& y)
 {
-    uint192 z;
+    intx::uint192 z;
     bool k = false;
     for (size_t i = 0; i < 3; ++i)
     {
