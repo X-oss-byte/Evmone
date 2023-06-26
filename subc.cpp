11e254b74918981a75e6ae47afe9b965543136c1
Paweł Bylica <pawel@ethereum.org>
2023-06-26 17:28:18 +0200

    subc_test reduce

--- subc.cpp
+++ subc.cpp
@@ -1,37 +1,17 @@
 using u64 = unsigned long long;
 
-struct result_with_carry
-{
-    u64 value;
-    u64 carry;
-};
-
-inline result_with_carry _subc(u64 x, u64 y, u64 carry) noexcept
-{
-    u64 carryout = 0;
-    const auto d = __builtin_subcll(x, y, carry, &carryout);
-    return {d, carryout};
-}
-
-
-[[gnu::noinline]] static auto neg(const u64* y)
+[[gnu::noinline]] static auto neg(const u64 y[3])
 {
     u64 k = 0;
-
     __builtin_subcll(0, y[0], k, &k);
-
-    auto t = _subc(0, y[1], k);
-    k = t.carry;
-    t = _subc(0, y[2], k);
-    return t.value;
+    __builtin_subcll(0, y[1], k, &k);
+    return __builtin_subcll(0, y[2], k, &k);
 }
 
 int main()
 {
     const u64 y[] = {1, 0, 0};
-    auto d = neg(y);
-    if (d != ~u64{})
+    if (neg(y) != ~u64(0))
         return 1;
-
     return 0;
 }
