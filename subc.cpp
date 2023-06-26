28ab50023cfc1a4c9915e085b29108dad9e552b3
Paweł Bylica <pawel@ethereum.org>
2023-06-26 17:15:58 +0200

    subc_test reduce

--- subc.cpp
+++ subc.cpp
@@ -5,21 +5,17 @@ struct result_with_carry
 {
     T value;
     bool carry;
-
-    /// Conversion to tuple of references, to allow usage with std::tie().
-    constexpr operator std::tuple<T&, bool&>() noexcept { return {value, carry}; }
 };
 
-inline result_with_carry<uint64_t> _subc(
-    uint64_t x, uint64_t y, bool carry = false) noexcept
+inline result_with_carry<uint64_t> _subc(uint64_t x, uint64_t y, bool carry = false) noexcept
 {
-    unsigned long long carryout = 0;  // NOLINT(google-runtime-int)
+    unsigned long long carryout = 0;
     const auto d = __builtin_subcll(x, y, carry, &carryout);
     return {d, static_cast<bool>(carryout)};
 }
 
 
-[[gnu::noinline]] static auto neg(const intx::uint192& y)
+[[gnu::noinline]] static auto neg(const uint64_t* y)
 {
     bool k = false;
     auto t = _subc(0, y[0], k);
@@ -32,7 +28,8 @@ inline result_with_carry<uint64_t> _subc(
 
 int main()
 {
-    auto d = neg(1);
+    const uint64_t y[] = {1, 0, 0};
+    auto d = neg(y);
     if (d != ~uint64_t{})
         return 1;
 
